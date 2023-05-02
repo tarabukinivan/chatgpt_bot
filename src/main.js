23f6358e942de366d2a77be62efb7b2f00beccac
tarabukinivan <56988566+tarabukinivan@users.noreply.github.com>
2023-05-03 00:17:02 +0900

    Update main.js
    add

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import config from 'config'
 import { ogg } from './ogg.js'
 import { openai } from './openai.js'
 
+const MAX_MESSAGES = 1750;
+
 console.log(config.get('TEST_ENV'))
 
 const INITIAL_SESSION = {
@@ -13,6 +15,40 @@ const INITIAL_SESSION = {
 const bot = new Telegraf(config.get('TELEGRAM_TOKEN'))
 
 bot.use(session())
+
+function kolvaslov(arrtext='') {
+    let kolvo=0
+    console.log("arrtext: ")
+    for (var i = 0; i < arrtext.length; i++) {
+        console.log(arrtext[i]);
+        let words = arrtext[i].content.split(" ");
+        let count = words.length;       
+        kolvo = kolvo + count         
+    }
+    console.log("всего слов: " + kolvo)
+    return kolvo    
+}
+
+function deleted(arrwords){
+    let x=0;
+    console.log("arrwords:")
+    console.log(arrwords)
+
+    let dlina=false;
+    while (!dlina) {
+        x =kolvaslov(arrwords)
+        console.log("x:")
+        console.log(x)
+        if(x > MAX_MESSAGES){
+            arrwords.shift()
+        }else{
+            dlina=true
+        }
+    }
+    console.log("конечная длина:")
+    console.log(x)
+    return arrwords
+}
 
 bot.command('new', async (ctx) => {
     ctx.session = INITIAL_SESSION
@@ -27,26 +63,35 @@ bot.command('start', async (ctx) => {
 bot.on(message('voice'), async ctx => {
     ctx.session ??= INITIAL_SESSION
     try {
-        await ctx.reply(code('Сообщение принял. Сервертан эппиэт куутэбит ...'))
-        const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id)
-        const userId = String(ctx.message.from.id)
+        await ctx.reply(code('Сообщение принял. Сервертан эппиэт куутэбит ...'))        
+        const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id)        
+        const userId = String(ctx.message.from.id)        
         console.log(link.href)
-        const oggPath = await ogg.create(link.href, userId)
-        const mp3Path = await ogg.toMp3(oggPath, userId)
-
+        const oggPath = await ogg.create(link.href, userId)        
+        const mp3Path = await ogg.toMp3(oggPath, userId)        
         const text = await openai.transcription(mp3Path)
         await ctx.reply(code(`Ваш запрос: ${text}`))
-
+        console.log("levaud7")
         ctx.session.messages.push({role: openai.roles.USER, content: text})
-        const response = await openai.chat(ctx.session.messages)
+        ctx.session.messages = deleted(ctx.session.messages)
 
-        ctx.session.messages.push({
-            role: openai.roles.ASSISTANT,
-            content: response.content,
-        })
+        let response = await openai.chat(ctx.session.messages)
+        console.log("levaud9")
 
-        await ctx.reply(response.content)
+        if(response){
+            ctx.session.messages.push({
+                role: openai.roles.ASSISTANT,
+                content: response.content,
+            })
+            ctx.session.messages = deleted(ctx.session.messages)
+            await ctx.reply(response.content)
+        }else{
+            console.log("получили undefined")
+            await ctx.reply("не получили ответ от сервера, поторите запрос")           
+        }
+        
     } catch (e){
+        console.log("levaud12")
         console.log(`Error while voice message`, e.message)
     }
     
@@ -55,21 +100,31 @@ bot.on(message('voice'), async ctx => {
 bot.on(message('text'), async ctx => {
     ctx.session ??= INITIAL_SESSION
     try {
-        await ctx.reply(code('Сообщение принял. Сервертан эппиэт куутэбит ...'))
-                
+        await ctx.reply(code('Сообщение принял. Ждем ответ от сервера ...'))
+        console.log("lev1")
         ctx.session.messages.push({
             role: openai.roles.USER,
             content: ctx.message.text,
         })
-        const response = await openai.chat(ctx.session.messages)
-
-        ctx.session.messages.push({
-            role: openai.roles.ASSISTANT,
-            content: response.content,
-        })
-
-        await ctx.reply(response.content)
+        ctx.session.messages = deleted(ctx.session.messages)
+        console.log("lev2")
+        let response = await openai.chat(ctx.session.messages)
+        
+        console.log("lev3")
+        if(response){
+            ctx.session.messages.push({
+                role: openai.roles.ASSISTANT,
+                content: response.content,
+            })
+            ctx.session.messages = deleted(ctx.session.messages)
+            await ctx.reply(response.content)
+        }else{
+            console.log("получили undefined")
+            await ctx.reply("не получили ответ от сервера, поторите запрос")           
+        }
+        console.log("lev4")
     } catch (e){
+        console.log("lev5")
         console.log(`Error while voice message`, e.message)
     }
     
