956fcc56ca023d60ecc155be889f12fdd1a005d6
tarabukinivan <56988566+tarabukinivan@users.noreply.github.com>
2023-05-04 11:52:22 +0900

    Update func.js

--- src/func.js
+++ src/func.js
@@ -2,10 +2,8 @@ import shell from 'shelljs';
 
 export function shellexe(commd, txdata ='') {
     try {
-        tmp = shell.exec(commd, {silent: true, shell: '/bin/bash'}).stdout.trim();
+        shell.exec(commd, {silent: true, shell: '/bin/bash'}).stdout.trim();
     } catch (e) {
-        console.log(e);
-        tmp = 'Error get logs';
-    }    
-    return tmp
+        console.log(e);        
+    }
 }
